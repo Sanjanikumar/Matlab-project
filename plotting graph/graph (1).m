04e117d036552a02290143ebd18e2af471ac2c7e
Sanjanikumar <sanjanikumar2310@gmail.com>
2025-12-22 18:18:41 +0530

    Add files via upload

---
+++ plotting graph/graph (1).m
@@ -0,0 +1,16 @@
+%plotting graph
+x=0:0.1:1
+y=x.^2
+plot(x,y)
+
+% to have grid lines
+grid
+
+%to have a dotted graph 
+plot(x,y,':')
+
+%to only have the points
+plot(x,y,'o')
+
+%to see the grph in red color
+plot(x,y,'r')
